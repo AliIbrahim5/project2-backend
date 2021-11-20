75a3335c1665acc6c4476c4e76d62c929a825205
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-20 17:44:41 +0300

    mongoose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const  express = require("express");
 const  dotenv = require("dotenv");
 const app = express();
+const db = require("./db/db");
 dotenv.config();
 app.use(express.json());
 const PORT = process.env.PORT
